2298b276732fdf83ff7027f99f2620e502b3d2ce
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2023-12-22 14:07:03 -0600

    sidebar github added

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -3,7 +3,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import './sidebar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
  <div className='nav-bar'>
@@ -44,6 +44,15 @@ const Sidebar = () => (
              href='https://www.linkedin.com/in/naveen-ramanjulu-01475073/'
             >
                 <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+            </a>
+        </li>
+        <li>
+            <a
+             target='_blank' 
+             rel='noreferrer'
+             href='https://github.com/nram76?tab=repositories'
+            >
+                <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
             </a>
         </li>
     </ul>
