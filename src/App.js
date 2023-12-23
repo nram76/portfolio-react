51ad2bf33f146374f0c55a890070d362e36d6fc0
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2023-12-23 11:40:45 -0600

    animated letters to home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/layout'
-import Home from './components/Home/home';
+import Home from './components/Home/home'; 
 
 function App() {
   return (
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom';
 import './home.scss'
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters/animatedLetters';
 
 const Home = () => {
+    // useState for setting variables
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['n','a','v','e','e','n'];
+    const jobArray = ['s','o','f','t','w','a','r','e',' ','d','e','v','e','l','o','p','e','r'];
+
+
     
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Naveen Ramanjulu
-                <br />
-                Software Engineer
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={1}
+                    />
+                    <br/>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={17}
+                    />
+                    
                 </h1>
                 <h2>FrontEnd Developer / JavaScript Expert / Nest Js Servers / Stripe / Openai / Azure DevOps</h2>
                 <Link to='/contact' className='flat-button' >Contact Me</Link>
